43bb6909166e3332e4af15c76e1e67f04001ffef
Usman Mughal <35332218+usmanmughalji@users.noreply.github.com>
2020-05-10 00:34:18 +0500

    Create up.sh

---
+++ up.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+if [ -e kernel/out/arch/arm64/boot/Image.gz-dtb ]; then
+	target=kernel/out/arch/arm64/boot/Image.gz-dtb;
+	echo
+	echo "File Found";
+	echo
+	echo "Uploading File Now"
+	echo
+	
+curl --upload-file kernel/out/arch/arm64/boot/Image.gz-dtb https://filepush.co/upload/Image.gz-dtb
+echo
+curl -F "file=@kernel/out/arch/arm64/boot/Image.gz-dtb" https://file.io
+echo
+echo
+curl -F'file=@kernel/out/arch/arm64/boot/Image.gz-dtb' https://0x0.st
+echo
+echo "File Uploaded Successfully"
+else
+	echo "File Not Found";
+
+fi
