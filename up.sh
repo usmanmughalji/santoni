6751f95e8d80f30b8a54768e46ab249c904af629
Usman Mughal <35332218+usmanmughalji@users.noreply.github.com>
2020-05-10 04:35:54 +0500

    Update up.sh

--- up.sh
+++ up.sh
@@ -1,19 +1,41 @@
 #!/bin/bash
 
+#Fire-Fox-Send Setup
+
+if [ -e usr/local/bin/ffsend ]; then
+	target=usr/local/bin/ffsend;
+	echo
+	echo "Fire Fox Send Already Installed";
+else
+
+wget https://raw.githubusercontent.com/usmanmughalji/build-scripts/master/ffsend
+chmod a+x ./ffsend
+sudo mv ./ffsend /usr/local/bin/
+
+fi
 
 if [ -e kernel/out/arch/arm64/boot/Image.gz-dtb ]; then
 	target=kernel/out/arch/arm64/boot/Image.gz-dtb;
 	echo
 	echo "File Found";
 	echo
-	echo "Uploading File Now"
+	echo "Uploading File Now";
 	echo
 	
-curl --upload-file kernel/out/arch/arm64/boot/Image.gz-dtb https://filepush.co/upload/Image.gz-dtb
+echo "Uploading To Fire Fox Send";
+ffsend upload kernel/out/arch/arm64/boot/Image.gz-dtb
+echo "Uploading Complete";
 echo
+echo "Uploading To File Push"
+curl --upload-file kernel/out/arch/arm64/boot/Image.gz-dtb https://filepush.co/upload/Image.gz-dtb
+echo "Uploading Complete";
+echo
+echo "Uploading To File.io"
 curl -F "file=@kernel/out/arch/arm64/boot/Image.gz-dtb" https://file.io
 echo
-echo "File Uploaded Successfully"
+echo "Uploading Complete";
+echo
+echo "File Successfully Uploaded To All Respective File Hosting Servers"
 else
 	echo "File Not Found";
 
